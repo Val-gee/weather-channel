56e57ac86cb294103f31b8f1991ea597f2f97a87
val.gee <vguevara0806@gmail.com>
2022-12-06 10:29:47 -0500

    started HTML sheet and file structure

---
+++ Develop/js/style.js
@@ -0,0 +1,11 @@
+//set initial array for cities
+
+//function to display buttons after adding a new city or pulled from our array of movies
+    //cleaning my cities from view
+    //looping through array for each movie stored
+        //dynamicaly generate buttons for each city in our array
+        //add a class to the button
+        //add a data-attrubute 
+        //providing text for the button
+        //adding the button to the BUTTONS-VIEW DIV
+
