8f6eb97d48adc1cb18fc260f4b56b5aa6459eedc
val.gee <vguevara0806@gmail.com>
2022-12-08 22:16:33 -0500

    added five day forecast from weather api and added bootstrap features in index.html

--- Develop/js/style.js
+++ Develop/js/style.js
@@ -9,6 +9,7 @@ var api_key = '7a15c3334d6b00f3714da34b31d34a10';
 var cities = [];
 // var searchHistory = [];
 var cityInfo = $("#searchedCityInfo");
+var fiveDayCards = $('.list-group');
 
 function displayCityInfo(cityname){
     $(cityInfo).empty();
@@ -22,44 +23,31 @@ function displayCityInfo(cityname){
     }).then(function(response) {
     console.log(response)
 
-    // var cityDiv = $("<div class='movie>");
-    // var cName = response.cityname;
-
     var cityTitle = $("<h1>").text(cityname);
-
-    cityInfo.append(cityTitle);
-
-    var wind = response.list[3].wind.speed;
-
     var p = $("<p>");
-
-    p.text("Wind Speed:" + " " + wind + " " + "mph");
-
-    cityInfo.append(p);
-
-    console.log(wind);
-
-    var humidity = response.list[3].main.humidity;
-
     var p2 = $("<p>");
-
-    p2.text("Humidity:" + " " + humidity + " " + "%");
-
-    cityInfo.append(p2);
-
-    console.log(humidity);
-
+    var p3 = $("<p>");
+    var wind = response.list[3].wind.speed;
+    var humidity = response.list[3].main.humidity;
     var temp = response.list[3].main.temp;
 
-    var p3 = $("<p>");
+    console.log(wind);
+    console.log(humidity);
+    console.log(temp);   
 
+    p.text("Wind Speed:" + " " + wind + " " + "mph");
+    p2.text("Humidity:" + " " + humidity + " " + "%");
     p3.text("Temp:" + " " + temp + " " + "°F");
 
+    
+    cityInfo.append(cityTitle);
+    cityInfo.append(p);
+    cityInfo.append(p2);
     cityInfo.append(p3);
 
-    console.log(temp);
-    })
-    
+
+    });
+
     displayHistory();
 }
 
@@ -68,7 +56,7 @@ function displayHistory() {
     for (var i = 0; i < cities.length; i++) {
         var a = $("<button>");
 
-        a.addClass("city-btn");
+        a.addClass("city-btn btn-primary");
 
         a.attr("data-name", cities[i]);
 
@@ -76,7 +64,43 @@ function displayHistory() {
 
         $('#history-view').append(a);
     }
-}
+};
+
+function displayFiveDayForecast(cityname) {
+    $(fiveDayCards).empty();
+
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${api_key}&units=${units}&lang=${lang}`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+    
+        for (var i = 0; i < fiveDayCards.length; i++) {
+            var currentDate = (response.list[i * 8].dt_txt);
+            var currentWind = response.list[i * 8].wind.speed;
+            var currentHumidity = response.list[i * 8].main.humidity;
+            var currentTemp = response.list[i * 8].main.temp;
+    
+            var d = $('<li>');
+            var w = $('<li>');
+            var h = $('<li>');
+            var t = $('<li>');
+    
+            d.text("Date:" + " " + currentDate);
+            w.text("Wind Speed:" + " " + currentWind + " " + "mph");
+            h.text("Humidity:" + " " + currentHumidity + " " + "%");
+            t.text("Temp:" + " " + currentTemp + " " + "F°")
+    
+            fiveDayCards.append(d);
+            fiveDayCards.append(w);
+            fiveDayCards.append(h);
+            fiveDayCards.append(t);
+    
+            }
+    });
+};
 
 $('#search-city').on('click', function(event) {
     event.preventDefault();
@@ -90,6 +114,8 @@ $('#search-city').on('click', function(event) {
     displayHistory();
 
     displayCityInfo(cityname);
+
+    displayFiveDayForecast(cityname);
 
 });
 
